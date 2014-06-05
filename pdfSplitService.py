a28369a0b0c2a4a61f4dfe47218c410a1c47baf0
Håkon Nymo Matland <Matland@Hakons-MacBook-Pro.local>
2014-06-05 15:39:44 +1000

    indentation error

--- pdfSplitService.py
+++ pdfSplitService.py
@@ -18,13 +18,13 @@ class split:
 
 	def POST(self):
 		x = web.input(myfile={})
-        directory = '/var/pdfSplit/pdf' # change this to the directory you want to store the file in.
+		directory = '/var/pdfSplit/pdf' # change this to the directory you want to store the file in.
         if 'myfile' in x: # to check if the file-object is created
         	filename = x['myfile'].filename
-            fout = open(directory +'/'+ filename,'w') # creates the file where the uploaded file should be stored
-            fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
-            fout.close() # closes the file, upload complete.
-        raise web.seeother('/upload')
+			fout = open(directory +'/'+ filename,'w') # creates the file where the uploaded file should be stored
+			fout.write(x.myfile.file.read()) # writes the uploaded file to the newly created file.
+			fout.close() # closes the file, upload complete.
+		raise web.seeother('/upload')
 
 if __name__ == '__main__':
 	app.run()
